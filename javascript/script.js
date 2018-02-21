453b0bb5500fa0ad95cedd31572de418b5b30c0c
Jozelle <jozellelauzon024@gmail.com>
2018-02-21 12:20:39 +0100

    aggiunta data scadenza

--- javascript/script.js
+++ javascript/script.js
@@ -1,23 +1,36 @@
 function caricaData(){
 
     setTimeout(function(){
-        moment('it');
-        var sunrise = 1485762037; // hour of sunrise in json openweathermap
 
+        moment.locale('it');
+        
+        var sunrise = 1485762037; // hour of sunrise in json openweathermap
+        var oggi = moment(); // date of today 
+        var scadenza = oggi.add(30, 'days');
+        console.log(oggi);
         var data = moment.unix(sunrise);
+
         var dataestesa = data.format('h:mm:ss'); //.format change the format in the following ()
         var dataintera = data.format('LLL');
         var datasettimana = data.format('LLLL')
+        var datascadenza = scadenza.format('LLLL');
+
         var p = document.createElement('p');
         var p1 = document.createElement('p');
         var p3 = document.createElement('p');
+        var p4 = document.createElement('p');
+
         p.innerText = dataestesa;
         p1.innerText = dataintera;
         p3.innerText = datasettimana;
+        p4.innerText = datascadenza;
+
         var h1 = document.getElementById('sezione');
+
         h1.appendChild(p);
         h1.appendChild(p1);
         h1.appendChild(p3);
+        h1.appendChild(p4);
 
     }, 1000) 
 
