46bb215dda9fd63d5271b29ceb2e5a314f90e04d
Jozelle <jozellelauzon024@gmail.com>
2018-02-21 11:54:52 +0100

    modificata la funzione caricaData

--- javascript/script.js
+++ javascript/script.js
@@ -1,14 +1,15 @@
 function caricaData(){
 
-    setInterval(function(){
+    setTimeout(function(){
         var sunrise = 1485762037; // hour of sunrise in json openweathermap
 
         var data = moment(sunrise);
         var dataestesa = moment().format('MMMM Do YYYY, h:mm:ss a'); //.format change the format in the following ()
-        console.log(dataestesa);
-        var h1 = document.getElementById('data');
-        h1.innerText = dataestesa;
-    },1000);
+        var p = document.createElement('p');
+        p.innerText = dataestesa;
+        var h1 = document.getElementById('sezione');
+        h1.appendChild(p);
 
+    }, 1000) 
 
-}
+};
