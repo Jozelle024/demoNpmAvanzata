8df693ff7987b262130de9cd4e7177de92be76ac
Jozelle <jozellelauzon024@gmail.com>
2018-02-21 12:09:03 +0100

    modificata la funzione caricaData, aggiunta dataintera e data settimana

--- javascript/script.js
+++ javascript/script.js
@@ -1,14 +1,23 @@
 function caricaData(){
 
     setTimeout(function(){
+        moment('it');
         var sunrise = 1485762037; // hour of sunrise in json openweathermap
 
-        var data = moment(sunrise);
-        var dataestesa = moment().format('MMMM Do YYYY, h:mm:ss a'); //.format change the format in the following ()
+        var data = moment.unix(sunrise);
+        var dataestesa = data.format('h:mm:ss'); //.format change the format in the following ()
+        var dataintera = data.format('LLL');
+        var datasettimana = data.format('LLLL')
         var p = document.createElement('p');
+        var p1 = document.createElement('p');
+        var p3 = document.createElement('p');
         p.innerText = dataestesa;
+        p1.innerText = dataintera;
+        p3.innerText = datasettimana;
         var h1 = document.getElementById('sezione');
         h1.appendChild(p);
+        h1.appendChild(p1);
+        h1.appendChild(p3);
 
     }, 1000) 
 
